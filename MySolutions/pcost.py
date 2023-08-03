93d52fcb4499d5fcf1c8519ad28aae0eaf3e589c
Andrei Anokhin <andrei_anokhin@epam.com>
2023-08-03 22:30:42 +0300

    ex1_4

--- MySolutions/pcost.py
+++ MySolutions/pcost.py
@@ -1,11 +1,24 @@
 file_path = '../Data/portfolio.dat'
 
-total_sum = 0
-with open(file_path, 'r') as file:
-    for line in file.readlines():
-        line_list = line.split()
-        num_shares = float(line_list[1])
-        price_shares = float(line_list[2])
-        total_sum += num_shares * price_shares
+def portfolio_cost(file_path):
+    total_sum = 0
+    with open(file_path, 'r') as file:
+        for line in file.readlines():
+            line_list = line.split()
+            try:
+                num_shares = float(line_list[1])
+            except ValueError as e:
+                print(f"Couldn't parse: {line_list[1]}")
+                print(f"Reason: {e}")
+            try:
+                price_shares = float(line_list[2])
+            except ValueError as e:
+                print(f"Couldn't parse: {line_list[2]}")
+                print(f"Reason: {e}")
+            total_sum += num_shares * price_shares
+    return total_sum
 
-print(total_sum)
+
+# print(portfolio_cost(file_path))
+file_path_bad_data = '../Data/portfolio3.dat'
+print(portfolio_cost(file_path_bad_data))
