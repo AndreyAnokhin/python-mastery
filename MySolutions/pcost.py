af94bd866ca264f5441b121b4c50561fb3f78cd6
Andrei Anokhin <andrei_anokhin@epam.com>
2023-08-03 22:37:32 +0300

    ex1_4 improve

--- MySolutions/pcost.py
+++ MySolutions/pcost.py
@@ -7,13 +7,9 @@ def portfolio_cost(file_path):
             line_list = line.split()
             try:
                 num_shares = float(line_list[1])
-            except ValueError as e:
-                print(f"Couldn't parse: {line_list[1]}")
-                print(f"Reason: {e}")
-            try:
                 price_shares = float(line_list[2])
             except ValueError as e:
-                print(f"Couldn't parse: {line_list[2]}")
+                print("Couldn't parse:", repr(line))
                 print(f"Reason: {e}")
             total_sum += num_shares * price_shares
     return total_sum
